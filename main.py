5e430579c6f15398ae894454274c99d593549cb1
Your Namejava <7474gege@gmail.com>
2023-01-17 21:31:58 -0500

    updates

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def on_message(message: discord.Message):
             await message.channel.send(embed=load)
             return await scrape(message)
         elif message.content.startswith('!help'):
-            help = discord.Embed(title="Help", description='Format: !scrape item=:"ItemName" location="Location" distance="Distance"(No Spaces)\nAvailable Distance:1,2,5,10,20,40,60,65,80,100,250,500 \nExample: !scrape item=iPhone12 location=NewYork distance=50"', color=0x00ff00)
+            help = discord.Embed(title="Help", description='Format: !scrape item=:"ItemName" location="Location" distance="Distance"(No Spaces)\nAvailable Distance:1,2,5,10,20,40,60,65,80,100,250,500 \nExample: !scrape item=iPhone12 location=NewYork distance=50', color=0x00ff00)
             return await message.channel.send(embed=help)
         else:
             error = discord.Embed(title="Error", description="Invalid Command", color=0xff0000)
